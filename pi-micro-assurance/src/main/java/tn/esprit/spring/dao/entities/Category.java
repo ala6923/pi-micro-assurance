2df2226c631c98c71c4b7f75c72737e8717b87d2
Meddhib <Dhib.Mohamed@esprit.tn>
2021-03-20 22:51:01 +0100

    test

--- pi-micro-assurance/src/main/java/tn/esprit/spring/dao/entities/Category.java
+++ pi-micro-assurance/src/main/java/tn/esprit/spring/dao/entities/Category.java
@@ -2,6 +2,6 @@ package tn.esprit.spring.dao.entities;
 
 public enum Category {
 	
-	life_insurance , agriculture_insurance
+	life_insurance , agriculture_insurance, health_insurance, income_insurance
 
 }
