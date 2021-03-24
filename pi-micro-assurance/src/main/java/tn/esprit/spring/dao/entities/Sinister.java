c319427df7abad947c922e9703c4184691cbb38e
OumaimaIchaouii <80512959+OumaimaIchaouii@users.noreply.github.com>
2021-03-24 12:18:46 +0100

    Update Sinister.java

--- pi-micro-assurance/src/main/java/tn/esprit/spring/dao/entities/Sinister.java
+++ pi-micro-assurance/src/main/java/tn/esprit/spring/dao/entities/Sinister.java
@@ -43,6 +43,101 @@ public class Sinister implements Serializable{
 	
 	@Column(name="sinisterType")
 	private String sinisterType;
+	public Sinister(int id, Date declarationDate, Date sinisterDate, String sinisterPlace, int sinisterNumber,
+			String sinisterDescription, String sinisterStatus, String sinisterType, Set<Amendment> amendments,
+			Set<Arrangment> arrangments) {
+		super();
+		this.id = id;
+		this.declarationDate = declarationDate;
+		this.sinisterDate = sinisterDate;
+		this.sinisterPlace = sinisterPlace;
+		this.sinisterNumber = sinisterNumber;
+		this.sinisterDescription = sinisterDescription;
+		this.sinisterStatus = sinisterStatus;
+		this.sinisterType = sinisterType;
+		this.amendments = amendments;
+		this.arrangments = arrangments;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Date getDeclarationDate() {
+		return declarationDate;
+	}
+
+	public Date getSinisterDate() {
+		return sinisterDate;
+	}
+
+	public String getSinisterPlace() {
+		return sinisterPlace;
+	}
+
+	public int getSinisterNumber() {
+		return sinisterNumber;
+	}
+
+	public String getSinisterDescription() {
+		return sinisterDescription;
+	}
+
+	public String getSinisterStatus() {
+		return sinisterStatus;
+	}
+
+	public String getSinisterType() {
+		return sinisterType;
+	}
+
+	public Set<Amendment> getAmendments() {
+		return amendments;
+	}
+
+	public Set<Arrangment> getArrangments() {
+		return arrangments;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setDeclarationDate(Date declarationDate) {
+		this.declarationDate = declarationDate;
+	}
+
+	public void setSinisterDate(Date sinisterDate) {
+		this.sinisterDate = sinisterDate;
+	}
+
+	public void setSinisterPlace(String sinisterPlace) {
+		this.sinisterPlace = sinisterPlace;
+	}
+
+	public void setSinisterNumber(int sinisterNumber) {
+		this.sinisterNumber = sinisterNumber;
+	}
+
+	public void setSinisterDescription(String sinisterDescription) {
+		this.sinisterDescription = sinisterDescription;
+	}
+
+	public void setSinisterStatus(String sinisterStatus) {
+		this.sinisterStatus = sinisterStatus;
+	}
+
+	public void setSinisterType(String sinisterType) {
+		this.sinisterType = sinisterType;
+	}
+
+	public void setAmendments(Set<Amendment> amendments) {
+		this.amendments = amendments;
+	}
+
+	public void setArrangments(Set<Arrangment> arrangments) {
+		this.arrangments = arrangments;
+	}
 	
 	@OneToMany(cascade = CascadeType.ALL)
 	private Set<Amendment> amendments;
