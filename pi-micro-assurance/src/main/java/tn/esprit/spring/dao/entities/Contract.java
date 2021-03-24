5cd80ab2e94851f38466392a7f4b96b98c6a7ca0
OumaimaIchaouii <80512959+OumaimaIchaouii@users.noreply.github.com>
2021-03-24 12:07:36 +0100

    Update Contract.java

--- pi-micro-assurance/src/main/java/tn/esprit/spring/dao/entities/Contract.java
+++ pi-micro-assurance/src/main/java/tn/esprit/spring/dao/entities/Contract.java
@@ -25,7 +25,8 @@ public class Contract implements Serializable {
 	private int id; // Clé primaire
 	
 	@Column(name="ContractType")
-	private String ContractType;
+	@Enumerated(EnumType.STRING)
+	Category category ;
 	
 	@Temporal (TemporalType.DATE)
 	private Date signDate;
@@ -65,6 +66,234 @@ public class Contract implements Serializable {
 	
 	@Column(name="QRCode")
 	private String QRCode;
+	public Contract(int id, Category category, Date signDate, Date deadLineDate, String police, double payedAmount,
+			double reminingAmount, double netPremiuim, double totalPemium, String status, double discount, double tax,
+			double comission, double reInsurancePart, String qRCode, Insurer insurer, Insured insured,
+			Set<Payment> payments, Set<Product> products, Set<Amendment> amendments) {
+		super();
+		this.id = id;
+		this.category = category;
+		this.signDate = signDate;
+		this.deadLineDate = deadLineDate;
+		Police = police;
+		this.payedAmount = payedAmount;
+		this.reminingAmount = reminingAmount;
+		this.netPremiuim = netPremiuim;
+		this.totalPemium = totalPemium;
+		this.status = status;
+		this.discount = discount;
+		this.tax = tax;
+		this.comission = comission;
+		this.reInsurancePart = reInsurancePart;
+		QRCode = qRCode;
+		this.insurer = insurer;
+		this.insured = insured;
+		this.payments = payments;
+		this.products = products;
+		this.amendments = amendments;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public Category getCategory() {
+		return category;
+	}
+
+
+	public Date getSignDate() {
+		return signDate;
+	}
+
+
+	public Date getDeadLineDate() {
+		return deadLineDate;
+	}
+
+
+	public String getPolice() {
+		return Police;
+	}
+
+
+	public double getPayedAmount() {
+		return payedAmount;
+	}
+
+
+	public double getReminingAmount() {
+		return reminingAmount;
+	}
+
+
+	public double getNetPremiuim() {
+		return netPremiuim;
+	}
+
+
+	public double getTotalPemium() {
+		return totalPemium;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public double getDiscount() {
+		return discount;
+	}
+
+
+	public double getTax() {
+		return tax;
+	}
+
+
+	public double getComission() {
+		return comission;
+	}
+
+
+	public double getReInsurancePart() {
+		return reInsurancePart;
+	}
+
+
+	public String getQRCode() {
+		return QRCode;
+	}
+
+
+	public Insurer getInsurer() {
+		return insurer;
+	}
+
+
+	public Insured getInsured() {
+		return insured;
+	}
+
+
+	public Set<Payment> getPayments() {
+		return payments;
+	}
+
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+
+	public Set<Amendment> getAmendments() {
+		return amendments;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+
+	public void setSignDate(Date signDate) {
+		this.signDate = signDate;
+	}
+
+
+	public void setDeadLineDate(Date deadLineDate) {
+		this.deadLineDate = deadLineDate;
+	}
+
+
+	public void setPolice(String police) {
+		Police = police;
+	}
+
+
+	public void setPayedAmount(double payedAmount) {
+		this.payedAmount = payedAmount;
+	}
+
+
+	public void setReminingAmount(double reminingAmount) {
+		this.reminingAmount = reminingAmount;
+	}
+
+
+	public void setNetPremiuim(double netPremiuim) {
+		this.netPremiuim = netPremiuim;
+	}
+
+
+	public void setTotalPemium(double totalPemium) {
+		this.totalPemium = totalPemium;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+
+
+	public void setComission(double comission) {
+		this.comission = comission;
+	}
+
+
+	public void setReInsurancePart(double reInsurancePart) {
+		this.reInsurancePart = reInsurancePart;
+	}
+
+
+	public void setQRCode(String qRCode) {
+		QRCode = qRCode;
+	}
+
+
+	public void setInsurer(Insurer insurer) {
+		this.insurer = insurer;
+	}
+
+
+	public void setInsured(Insured insured) {
+		this.insured = insured;
+	}
+
+
+	public void setPayments(Set<Payment> payments) {
+		this.payments = payments;
+	}
+
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+
+	public void setAmendments(Set<Amendment> amendments) {
+		this.amendments = amendments;
+	}
+
+
 	
 	@ManyToOne(cascade = CascadeType.ALL)
 	Insurer insurer;
