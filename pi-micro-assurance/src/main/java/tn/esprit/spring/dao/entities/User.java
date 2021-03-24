5fd7a1a75682da91576cfed1a035602bd1443cdb
OumaimaIchaouii <80512959+OumaimaIchaouii@users.noreply.github.com>
2021-03-24 12:22:20 +0100

    Update User.java

--- pi-micro-assurance/src/main/java/tn/esprit/spring/dao/entities/User.java
+++ pi-micro-assurance/src/main/java/tn/esprit/spring/dao/entities/User.java
@@ -53,47 +53,104 @@ public  class User implements Serializable{
 	
 	
 	
-	public User(){
-		
-	}
-	
-public User ( int id ,String name , String firstname ,int Cin ,String email, int phoneNumber ,String adress,String login ,String password,String status){
-		
-		
-		this.id=id;
-		this.name=name ;
-		this.firstname=firstname ;
-		this.Cin=Cin;
-		this.email=email;
-		this.phoneNumber=phoneNumber ;
-		this.adress=adress ;
-		this.login=login;
-		this.password=password ;
-		this.status=status;
+	public User(int id, String name, String firstname, int cin, String email, int phoneNumber, String adress,
+			String login, String password, String status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.firstname = firstname;
+		Cin = cin;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.adress = adress;
+		this.login = login;
+		this.password = password;
+		this.status = status;
 	}
 
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
-public User ( String name , String firstname ,int Cin ,String email, int phoneNumber ,String adress,String login ,String password,String status){
-	
-	
-	
-	this.name=name ;
-	this.firstname=firstname ;
-	this.Cin=Cin;
-	this.email=email;
-	this.phoneNumber=phoneNumber ;
-	this.adress=adress ;
-	this.login=login;
-	this.password=password ;
-	this.status=status;
-}
-	
+	public int getId() {
+		return id;
+	}
 
-public User(String name,String firstname){
-	
-	this.name=name ;
-	this.firstname=firstname ;
-	
-}
+	public String getName() {
+		return name;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public int getCin() {
+		return Cin;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public void setCin(int cin) {
+		Cin = cin;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 
 }
