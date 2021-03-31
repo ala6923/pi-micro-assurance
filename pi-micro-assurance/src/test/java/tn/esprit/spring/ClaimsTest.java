65ba16444e920b00dc206b31e5e98c6434767b4d
Meddhib <Dhib.Mohamed@esprit.tn>
2021-03-31 16:15:22 +0200

    djmedamlkjslk

--- pi-micro-assurance/src/test/java/tn/esprit/spring/ClaimsTest.java
+++ pi-micro-assurance/src/test/java/tn/esprit/spring/ClaimsTest.java
@@ -1,21 +1,23 @@
 package tn.esprit.spring;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.dao.entities.Claims;
 import tn.esprit.spring.repository.ClaimRepository;
-
-@DataJpaTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+//@DataJpaTest
 public class ClaimsTest {
 	@Autowired
 	private ClaimRepository cr;
-	@Test
-	public void creatClaim(){
-		Claims cl=new Claims(1, null, 1, null);
-		cr.save(cl);
-		
-	} 
+	
 
 }
