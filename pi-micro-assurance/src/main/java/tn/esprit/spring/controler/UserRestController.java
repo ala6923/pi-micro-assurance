08924f45087f7e06420fbbb30a991bb714a04b20
oumaima <oumaima.ichaoui1@esprit.tn>
2021-04-05 14:34:26 +0100

    oumaima

---
+++ pi-micro-assurance/src/main/java/tn/esprit/spring/controler/UserRestController.java
@@ -0,0 +1,50 @@
+package tn.esprit.spring.controler;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.dao.entities.User;
+import tn.esprit.spring.service.UserServiceInterface;
+
+@RestController
+public class UserRestController {
+	@Autowired
+	UserServiceInterface us;
+	@PostMapping("/add-user")
+	@ResponseBody
+	public User addUser(@RequestBody User u) {
+	User user = us.addUser(u);
+	return user;
+
+	}
+	@DeleteMapping("/remove-user/{user-id}")
+	@ResponseBody
+	public void deleteUser(@PathVariable("user-id") int id) {
+	us.deleteUser(id);
+	}
+	@PutMapping("/modify-user")
+	@ResponseBody
+	public User modifyUser(@RequestBody User user) {
+	return us.updateUser(user);
+	}
+	@GetMapping("/retrieve-user/{user-id}")
+	@ResponseBody
+	public User retrieveUser(@PathVariable("user-id") int id) {
+	return us.retrieveUser(id);
+	}
+	@GetMapping("/retrieve-all-users")
+	@ResponseBody
+	public List<User> retrieveAllUsers() {
+	List<User> list = us.retrieveAllUsers();
+	return list;
+	}}
+
