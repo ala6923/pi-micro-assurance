d812dffeae96d948bcf626b33604eec03a3cce1d
oumaima <oumaima.ichaoui1@esprit.tn>
2021-04-07 22:26:20 +0100

    oumaima

--- pi-micro-assurance/src/main/java/tn/esprit/spring/controler/UserRestController.java
+++ pi-micro-assurance/src/main/java/tn/esprit/spring/controler/UserRestController.java
@@ -1,15 +1,27 @@
 package tn.esprit.spring.controler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,5 +60,30 @@ public class UserRestController {
 	public List<User> retrieveAllUsers() {
 	List<User> list = us.retrieveAllUsers();
 	return list;
-	}}
+	}
+	@RequestMapping("/user")
+
+	
+		@GetMapping
+		public ResponseEntity<User> getUser(){
+
+			User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+			return  new ResponseEntity<User>(user, HttpStatus.OK);
+		}
+		@RequestMapping(value="/getlogedUser")
+		public Map<String,Object> getLogedUser(HttpServletRequest httpServletRequest ){
+	//injecter la session courante
+			HttpSession httpSession=httpServletRequest.getSession();
+			SecurityContext securityContext= (SecurityContext) httpSession.getAttribute("SPRING_SECURITY_CONTEXT");
+			String username=securityContext.getAuthentication().getName();
+			List<String> roles= new ArrayList<>();
+			for (GrantedAuthority ga :securityContext.getAuthentication().getAuthorities()){
+				roles.add(ga.getAuthority());} //granted authority:users allowed
+			Map<String,Object> params=new HashMap<>();//objet type map
+			params.put("Username", username);
+			params.put("roles", roles);
+			return params;
+			}
+	}
 
