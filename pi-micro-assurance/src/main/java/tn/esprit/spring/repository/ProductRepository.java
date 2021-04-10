5dc1ab79a7fd1fa8d98a8e2323aa69a5789df060
sahnounmanel96 <73542174+sahnounmanel96@users.noreply.github.com>
2021-04-10 03:44:59 +0200

    Update ProductRepository.java

--- pi-micro-assurance/src/main/java/tn/esprit/spring/repository/ProductRepository.java
+++ pi-micro-assurance/src/main/java/tn/esprit/spring/repository/ProductRepository.java
@@ -1,6 +1,10 @@
 package tn.esprit.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.dao.entities.Product;
@@ -8,4 +12,17 @@ import tn.esprit.spring.dao.entities.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long>{
 
+	@Query("SELECT p FROM Product p WHERE p.name_ins= :name_ins")
+	List<Product> retrieveProductsByName_ins(@Param("name_ins") Name_ins name_ins);
+
+	
+	@Modifying
+	@Query("update User p set p.id = :role where p. = :id")
+	int updateProductStatusById(@Param("id") Id id, @Param("id1") String id1);
+	
+	@Modifying
+	@Query(value = "INSERT INTO T_PRODUCT (id, name_ins, scale) VALUES (:i, :ns, :scale)", nativeQuery = true)
+	void insertProduct(@Param("id") String id, @Param("ns") Integer ns, @Param("scale") Integer scale);
+	
+	
 }
